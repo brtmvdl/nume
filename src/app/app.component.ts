6ac15c018cce9e86e7c35544c96fa0a8b43a4d75
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-18 21:49:32 -0300

    Lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { LocalService } from './services/local.service';
+import { Component, OnInit } from '@angular/core'
+import { LocalService } from './services/local.service'
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,6 @@ import { LocalService } from './services/local.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
   LINES_NUMBER = 4;
   COLUMNS_NUMBER = 4;
   SELECTED_NUMBERS = 4;
@@ -22,132 +21,132 @@ export class AppComponent implements OnInit {
   points = 0;
   max = 0;
 
-  constructor(
-    private local: LocalService,
+  // eslint-disable-next-line no-useless-constructor
+  constructor (
+    private local: LocalService
   ) {
   }
 
-  get gameMessageStyle(): {} {
+  get gameMessageStyle (): {} {
     return {
-      display: this.playing ? 'none' : 'block',
+      display: this.playing ? 'none' : 'block'
     }
   }
 
-  ngOnInit(): void {
-    this.setMax();
-    this.play();
+  ngOnInit (): void {
+    this.setMax()
+    this.play()
   }
 
-  randomNumber(max: number): number {
-    return Math.floor(Math.random() * max);
+  randomNumber (max: number): number {
+    return Math.floor(Math.random() * max)
   }
 
-  hasIn(which: number, list: number[]): boolean {
-    return list.some((num: number) => num === which);
+  hasIn (which: number, list: number[]): boolean {
+    return list.some((num: number) => num === which)
   }
 
-  setMax(): void {
-    const points = Number.parseInt(this.local.retrieve('points'), 10);
-    this.max = points ? points : 0;
+  setMax (): void {
+    const points = Number.parseInt(this.local.retrieve('points'), 10)
+    this.max = points || 0
   }
 
-  makePoints(): void {
-    this.points++;
+  makePoints (): void {
+    this.points++
     this.max = Math.max(this.max, this.points)
   }
 
-  pushInSelecteds(cell: number): void {
-    this.selecteds.push(cell);
+  pushInSelecteds (cell: number): void {
+    this.selecteds.push(cell)
   }
 
-  select(cell: number): void {
+  select (cell: number): void {
     if (this.hasIn(cell, this.selecteds)) {
-      this.lose();
-      return;
+      this.lose()
+      return
     }
 
-    this.makePoints();
-    this.pushInSelecteds(cell);
-    this.makeGame();
+    this.makePoints()
+    this.pushInSelecteds(cell)
+    this.makeGame()
   }
 
-  lose(): void {
-    this.local.set('points', this.max.toString());
-    this.selecteds = [];
-    this.playing = false;
+  lose (): void {
+    this.local.set('points', this.max.toString())
+    this.selecteds = []
+    this.playing = false
   }
 
-  randomNumbers(lenth: number, max: number): number[] {
-    const numbers: number[] = [];
-    while (lenth--) numbers.push(this.randomNumber(max));
-    return numbers;
+  randomNumbers (lenth: number, max: number): number[] {
+    const numbers: number[] = []
+    while (lenth--) numbers.push(this.randomNumber(max))
+    return numbers
   }
 
-  getNewNumbers(length: number, notIn: number[] = []): number[] {
-    const numbers = [];
+  getNewNumbers (length: number, notIn: number[] = []): number[] {
+    const numbers = []
 
     for (let i = 0; i < length; i++) {
-      let num: number;
+      let num: number
 
-      do num = this.randomNumber(90) + 10;
-      while (this.hasIn(num, numbers) || this.hasIn(num, notIn));
+      do num = this.randomNumber(90) + 10
+      while (this.hasIn(num, numbers) || this.hasIn(num, notIn))
 
-      numbers.push(num);
+      numbers.push(num)
     }
 
-    return numbers;
+    return numbers
   }
 
-  createMap(lines, columns): any[][] {
-    return [...Array(lines)].map(() => [...Array(columns)]);
+  createMap (lines, columns): any[][] {
+    return [...Array(lines)].map(() => [...Array(columns)])
   }
 
-  makeTable(lines: number, columns: number, numbers: number[] = []): number[][] {
+  makeTable (lines: number, columns: number, numbers: number[] = []): number[][] {
     const table = this.createMap(lines, columns)
-    let n = 0;
+    let n = 0
 
-    for (let l = 0; l < lines; l++)
-      for (let c = 0; c < columns; c++)
-        table[l][c] = numbers[n++];
+    for (let l = 0; l < lines; l++) {
+      for (let c = 0; c < columns; c++) { table[l][c] = numbers[n++] }
+    }
 
-    return table;
+    return table
   }
 
-  randomizeNumbers(numbers: number[]): number[] {
+  randomizeNumbers (numbers: number[]): number[] {
     return numbers
       .sort(() => Math.floor(Math.random() * 3) - 1)
       .sort(() => Math.floor(Math.random() * 3) - 1)
   }
 
-  randomizeAndFilter(list: number[], length: number): number[] {
+  randomizeAndFilter (list: number[], length: number): number[] {
     return this.randomizeNumbers(list)
-      .filter((_, ix) => ix < length);
+      .filter((_, ix) => ix < length)
   }
 
-  getGameNumbers(length: number, selectedsLength: number): number[] {
+  getGameNumbers (length: number, selectedsLength: number): number[] {
     const { selecteds } = this
     const filteredSelecteds = this.randomizeAndFilter(selecteds, selectedsLength)
-    const newNumbers = this.getNewNumbers(length - filteredSelecteds.length, selecteds);
+    const newNumbers = this.getNewNumbers(length - filteredSelecteds.length, selecteds)
     const numbers = this.randomizeNumbers(newNumbers.concat(filteredSelecteds))
 
-    return numbers;
+    return numbers
   }
 
-  makeGame(): void {
+  makeGame (): void {
     const {
       LINES_NUMBER: lines,
       COLUMNS_NUMBER: cols,
       SELECTED_NUMBERS: selecteds
     } = this
 
-    this.numbers = this.getGameNumbers(lines * cols, selecteds);
-    this.table = this.makeTable(lines, cols, this.numbers);
+    this.numbers = this.getGameNumbers(lines * cols, selecteds)
+    this.table = this.makeTable(lines, cols, this.numbers)
   }
 
-  play(): void {
-    this.makeGame();
-    this.points = 0;
-    this.playing = true;
+  play (): void {
+    this.makeGame()
+    this.points = 0
+    this.playing = true
   }
-
 }
--- src/app/services/local.service.ts
+++ src/app/services/local.service.ts
@@ -1,24 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class LocalService {
-
-  retrieveRaw(key: string): string {
-    return window.localStorage.getItem(key);
+  retrieveRaw (key: string): string {
+    return window.localStorage.getItem(key)
   }
 
-  retrieve(key: string): string {
-    return JSON.parse(this.retrieveRaw(key));
+  retrieve (key: string): string {
+    return JSON.parse(this.retrieveRaw(key))
   }
 
-  setRaw(key: string, value: string): void {
-    window.localStorage.setItem(key, value);
+  setRaw (key: string, value: string): void {
+    window.localStorage.setItem(key, value)
   }
 
-  set(key: string, value: any = ''): void {
-    this.setRaw(key, JSON.stringify(value));
+  set (key: string, value: any = ''): void {
+    this.setRaw(key, JSON.stringify(value))
   }
-
 }
